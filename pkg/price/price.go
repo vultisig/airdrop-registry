27bf315fbf2f7a9383f1dbd0a61a206984746beb
Johnny Luo <johnnyluo1980@gmail.com>
2024-08-13 17:16:37 +1000

    remove unused code

--- pkg/price/price.go
+++ pkg/price/price.go
@@ -1,23 +1,6 @@
 package price
 
-import (
-	"fmt"
-)
-
 func FetchPrice(chain, token string) (float64, error) {
 	// if token starts with 0x and length is 42, then it's a contract address
-	if len(token) == 42 && token[:2] == "0x" {
-		price, err := fetchCoinGeckoTokenPrice(chain, token)
-		if err != nil {
-			return 0, fmt.Errorf("fetchCoinGeckoTokenPrice failed: %v", err)
-		}
-		return price, nil
-	}
-
-	price, err := fetchCoinGeckoPrice(chain, token)
-	if err != nil {
-		return 0, fmt.Errorf("fetchCoinGeckoPrice failed: %v", err)
-	}
-
-	return price, nil
+	return 0, nil
 }
