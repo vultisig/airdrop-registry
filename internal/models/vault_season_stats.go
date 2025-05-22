48954e398aa7f1d9184799bcc68ee71a39334e4b
aminsato <amin.saradar@yahoo.com>
2025-05-22 13:14:08 +0330

    Store vault stats at end of each season

---
+++ internal/models/vault_season_stats.go
@@ -0,0 +1,20 @@
+package models
+
+import "gorm.io/gorm"
+
+// Store vault rank and points for each season
+type VaultSeasonStats struct {
+	gorm.Model
+	VaultID       uint    `gorm:"type:bigint;not null;uniqueIndex:vault_season_idx" json:"vault_id"`
+	SeasonID      uint    `gorm:"type:bigint;not null;uniqueIndex:vault_season_idx" json:"season_id"`
+	Rank          int64   `json:"rank"` // rank of the vault
+	Points        float64 `json:"points"`
+	Balance       int64   `gorm:"type:bigint;default:0" json:"balance"` // latest balance of the vault
+	LPValue       int64   `gorm:"type:bigint;default:0" json:"lp_value"`
+	SwapVolume    float64 `gorm:"type:bigint;default:0" json:"swap_volume"`
+	ReferralCount int64   `gorm:"type:bigint;default:0" json:"referral_count"`
+}
+
+func (*VaultSeasonStats) TableName() string {
+	return "vault_season_stats"
+}
