cae3e85839a081bbce597c5d83e31a4382859efa
aminsato <amin.saradar@yahoo.com>
2025-06-15 14:19:59 +0330

    Update unit test

--- internal/models/job_test.go
+++ internal/models/job_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/vultisig/airdrop-registry/internal/models"
 )
 
 // add new test for Job struct
@@ -14,14 +15,14 @@ func TestJobEpoch(t *testing.T) {
 		Multiplier: 1,
 	}
 	expectedStartOfEpoch := time.Date(2023, 10, 4, 0, 0, 0, 0, time.UTC).Unix()
-	assert.Equal(t, job.StartOfEpoch(), expectedStartOfEpoch)
+	assert.Equal(t, models.StartOfEpoch(job.JobDate), expectedStartOfEpoch)
 
 	expectedEndOfEpoch := time.Date(2023, 10, 5, 0, 0, 0, 0, time.UTC).Unix()
-	assert.Equal(t, job.EndOfEpoch(), expectedEndOfEpoch)
+	assert.Equal(t, models.EndOfEpoch(job.JobDate), expectedEndOfEpoch)
 
 	job.Multiplier = 2
 
 	expectedStartOfEpoch = time.Date(2023, 10, 3, 0, 0, 0, 0, time.UTC).Unix()
-	assert.Equal(t, job.StartOfEpoch(), expectedStartOfEpoch)
-	assert.Equal(t, job.EndOfEpoch(), expectedEndOfEpoch)
+	assert.Equal(t, models.StartOfEpoch(job.JobDate), expectedStartOfEpoch)
+	assert.Equal(t, models.EndOfEpoch(job.JobDate), expectedEndOfEpoch)
 }
