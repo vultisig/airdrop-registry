aa8ea8edf64e588b6e356e22f3e82f4b5ed3cedc
aminsato <amin.saradar@yahoo.com>
2025-06-16 09:52:03 +0330

    Fix unit test

--- internal/models/job_test.go
+++ internal/models/job_test.go
@@ -10,18 +10,11 @@ import (
 // add new test for Job struct
 func TestJobEpoch(t *testing.T) {
 	job := &Job{
-		JobDate:    time.Date(2023, 10, 5, 5, 4, 3, 2, time.UTC),
-		Multiplier: 1,
+		JobDate: time.Date(2023, 10, 5, 5, 4, 3, 2, time.UTC),
 	}
 	expectedStartOfEpoch := time.Date(2023, 10, 4, 0, 0, 0, 0, time.UTC).Unix()
 	assert.Equal(t, StartOfEpoch(job.JobDate), expectedStartOfEpoch)
 
 	expectedEndOfEpoch := time.Date(2023, 10, 5, 0, 0, 0, 0, time.UTC).Unix()
-	assert.Equal(t, EndOfEpoch(job.JobDate), expectedEndOfEpoch)
-
-	job.Multiplier = 2
-
-	expectedStartOfEpoch = time.Date(2023, 10, 3, 0, 0, 0, 0, time.UTC).Unix()
-	assert.Equal(t, StartOfEpoch(job.JobDate), expectedStartOfEpoch)
 	assert.Equal(t, EndOfEpoch(job.JobDate), expectedEndOfEpoch)
 }
